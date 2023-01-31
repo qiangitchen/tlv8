69366d023f3b3644d48c345a68eb4de36db2b2b7
陈乾 <qianpou@163.com>
2023-01-31 21:32:21 +0800

    资源配置更新

--- tlv8/src/main/java/test/server/TLv8Application.java
+++ tlv8/src/main/java/test/server/TLv8Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * 启动测试服务主程序
@@ -11,8 +12,9 @@ import org.springframework.context.annotation.ComponentScans;
  * @author chenqian
  *
  */
-@ComponentScans(value = { @ComponentScan(value = "com.tlv8"), @ComponentScan(value = "test.server") })
 @SpringBootApplication
+@ComponentScans(value = { @ComponentScan(value = "com.tlv8"), @ComponentScan(value = "test.server") })
+@ImportResource("spring-mybatis.xml")
 public class TLv8Application {
 	public static void main(String[] args) {
 		SpringApplication.run(TLv8Application.class, args);
