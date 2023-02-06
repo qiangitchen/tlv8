bea2257249e173bdab31c16058e23b50702c1137
陈乾 <qianpou@163.com>
2023-02-06 14:20:47 +0800

    spring boot add WebSocket config

--- tlv8/src/main/java/com/tlv8/layim/WebSocketServer.java
+++ tlv8/src/main/java/com/tlv8/layim/WebSocketServer.java
@@ -10,10 +10,13 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+import org.springframework.stereotype.Component;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+@Component
 @ServerEndpoint("/IM/websocket/{userid}")
 public class WebSocketServer {
 	// 静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
