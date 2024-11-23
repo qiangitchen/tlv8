25e6ac3948631bee9c5ae297a5e9d88e233f691b
陈乾 <qianpou@163.com>
2024-11-23 14:05:28 +0800

    更新机构路径信息

--- tlv8.opm/src/main/java/com/tlv8/opm/UpdateOrgPathAction.java
+++ tlv8.opm/src/main/java/com/tlv8/opm/UpdateOrgPathAction.java
@@ -2,19 +2,18 @@ package com.tlv8.opm;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.tlv8.base.db.DBUtils;
 import com.tlv8.base.ActionSupport;
+import com.tlv8.base.utils.StringUtils;
+import com.tlv8.system.pojo.SaOporg;
+import com.tlv8.system.service.SaOporgService;
 
 /**
  * 更新机构路径信息
@@ -30,77 +29,45 @@ public class UpdateOrgPathAction extends ActionSupport {
 	private String scode;
 	private String sname;
 
+	@Autowired
+	SaOporgService saOpOrgService;
+
 	@ResponseBody
-	@RequestMapping(value = "/updateOrgPathAction", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
+	@PostMapping(value = "/updateOrgPathAction", produces = "application/json;charset=UTF-8")
 	public Object execute() throws Exception {
-		SqlSession session = DBUtils.getSession("system");
-		Connection conn = null;
 		try {
-			conn = session.getConnection();
-			String upSQL = "update SA_OPORG set SFID=?,SFCODE=?,SFNAME=? where sID = ?";
 			String fid = "", fcode = "", fname = "";
-			if (sparent != null && !"".equals(sparent)) {
-				String sql = "select SFID,SFCODE,SFNAME from SA_OPORG where SID = ?";
-				PreparedStatement ps = conn.prepareStatement(sql);
-				ps.setString(1, sparent);
-				ResultSet rs = ps.executeQuery();
-				if (rs.next()) {
-					fid = rs.getString(1);
-					fcode = rs.getString(2);
-					fname = rs.getString(3);
+			if (StringUtils.isNotEmpty(sparent)) {
+				SaOporg parentOrg = saOpOrgService.selectByPrimaryKey(sparent);
+				if (parentOrg != null) {
+					fid = parentOrg.getSfid();
+					fcode = parentOrg.getSfcode();
+					fname = parentOrg.getSname();
 				}
-				DBUtils.closeConn(null, null, ps, rs);
 			}
-			String sql1 = "select SID,SCODE,SNAME,SORGKINDID from SA_OPORG where SID = ?";
-			PreparedStatement ps1 = conn.prepareStatement(sql1);
-			ps1.setString(1, rowid);
-			ResultSet rs1 = ps1.executeQuery();
-			if (rs1.next()) {
-				fid += "/" + rs1.getString(1) + "." + rs1.getString(4);
-				fcode += "/" + rs1.getString(2);
-				fname += "/" + rs1.getString(3);
+			SaOporg saOrg = saOpOrgService.selectByPrimaryKey(rowid);
+			if (saOrg != null) {
+				saOrg.setSfid(fid + "/" + saOrg.getSid() + "." + saOrg.getSorgkindid());
+				saOrg.setSfcode(fcode + "/" + saOrg.getScode());
+				saOrg.setSfname(fname + "/" + saOrg.getSname());
+				saOpOrgService.updateData(saOrg);
+				updateChildOrgPath(saOrg);
 			}
-			DBUtils.closeConn(null, null, ps1, rs1);
-			PreparedStatement ps2 = conn.prepareStatement(upSQL);
-			ps2.setString(1, fid);
-			ps2.setString(2, fcode);
-			ps2.setString(3, fname);
-			ps2.setString(4, rowid);
-			ps2.executeUpdate();
-			DBUtils.closeConn(null, null, ps2, null);
-			updataChild(session, rowid, fid, fcode, fname);
-			session.commit(true);
 		} catch (Exception e) {
-			session.rollback(true);
 			e.printStackTrace();
-		} finally {
-			DBUtils.closeConn(session, conn, null, null);
 		}
 		return this;
 	}
 
-	private void updataChild(SqlSession session, String sid, String fid, String fcode, String fname) throws Exception {
-		String sql = "select SID,SORGKINDID,SCODE,SNAME from SA_OPORG where SPARENT = ? ";
-		Connection conn = session.getConnection();
-		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setString(1, sid);
-		ResultSet rs = ps.executeQuery();
-		while (rs.next()) {
-			String upSQL = "update SA_OPORG set SFID=?,SFCODE=?,SFNAME=? where sID = ?";
-			String id = rs.getString(1);
-			String sfid = fid + "/" + rs.getString(1) + "." + rs.getString(2);
-			String sfcode = fcode + "/" + rs.getString(3);
-			String sfname = fname + "/" + rs.getString(4);
-			PreparedStatement ps2 = conn.prepareStatement(upSQL);
-			ps2.setString(1, sfid);
-			ps2.setString(2, sfcode);
-			ps2.setString(3, sfname);
-			ps2.setString(4, id);
-			ps2.executeUpdate();
-			DBUtils.closeConn(null, null, ps2, null);
-			updataChild(session, id, sfid, sfcode, sfname);
+	private void updateChildOrgPath(SaOporg porg) {
+		List<SaOporg> orgList = saOpOrgService.selectListByParentID(porg.getSid());
+		for (SaOporg org : orgList) {
+			org.setSfid(porg.getSfid() + "/" + org.getSid() + "." + org.getSorgkindid());
+			org.setSfcode(porg.getSfcode() + "/" + org.getScode());
+			org.setSfname(porg.getSfname() + "/" + org.getSname());
+			saOpOrgService.updateData(org);
+			updateChildOrgPath(org);
 		}
-		DBUtils.closeConn(null, null, ps, rs);
 	}
 
 	public void setRowid(String rowid) {
